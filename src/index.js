5ccb1d29b9a2d62827230e49cb315e70ea3fd07a
Dmitry Petralai <pdo.hardwork@yandex.ru>
2021-03-09 11:47:17 +0400

    feat: implemente the function brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let newStack = [];
+    let openingBracket = []; 
+    let closingBracket = [];
+    for (let j = 0; j < bracketsConfig.length; j++) {
+        openingBracket[j] = bracketsConfig[j][0];
+        closingBracket[j] = bracketsConfig[j][1];
+    }
+    for (let i = 0; i < str.length; i++) {
+        if ((newStack.length === 0) && (openingBracket.indexOf(str[i]) > -1)) {
+            newStack.push(str[i]);
+        } else if ((closingBracket.indexOf(str[i]) > -1) && (closingBracket.indexOf(str[i]) === openingBracket.indexOf(newStack[newStack.length - 1]))) {
+                newStack.pop();
+        } else if (openingBracket.indexOf(str[i]) > -1) {
+            newStack.push(str[i]);
+        } else {
+            return false
+        }
+    }
+    return (newStack.length === 0);
 }
